ac64bfdac8d23161326a7ebe0322f70f70e86cb5
Ayush Dixit <ele.ayush@hotmail.com>
2021-06-28 15:41:05 +0530

    Create ReverseString.java

---
+++ recurssion/ReverseString.java
@@ -0,0 +1,19 @@
+class ReverseString{
+
+  static char reverseTheString(String str, StringBuilder revstr, int index){
+    
+    if(index == str.length() - 1){
+      return str.charAt(index) ;
+    }
+    char c = reverseTheString(str, revstr, index + 1) ;
+    revstr.append(c)  ;
+    return str.charAt(index) ;
+  }
+
+  public static void main(String[] args){
+    String str = "Ayush" ;
+    StringBuilder revstr = new StringBuilder() ;
+    reverseTheString(str,revstr,0) ;
+    System.out.println(revstr) ;
+  }
+}
